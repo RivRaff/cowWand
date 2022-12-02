2b2cd4482f4f2293625c68ee18ae068311cc0b48
RivRaff <109834013+RivRaff@users.noreply.github.com>
2022-12-02 15:58:33 -0600

    Initial commit

---
+++ src/main/java/rivraff/com/github/cowwand/FireCowCommand.java
@@ -0,0 +1,14 @@
+package rivraff.com.github.cowwand;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FireCowCommand  implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+
+    }
+}
